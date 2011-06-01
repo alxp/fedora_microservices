1415db96c700a88101670215189cf165abe3dbb8
jesterhazy <jonathan.esterhazy@gmail.com>
2011-06-01 20:45:33 -0300

    copied category.py changes from alex, fixed debug line

--- src/content_model_listeners/categories.py
+++ src/content_model_listeners/categories.py
@@ -27,19 +27,21 @@ def get_datastream_as_file(obj, dsid, extension = ''):
 
 def update_datastream(obj, dsid, filename, label='', mimeType='', controlGroup='M'): 
     # Using curl due to an incompatibility with the pyfcrepo library.
+    logging.debug("Updating datastream:")
     conn = obj.client.api.connection
+
     try:
-        logging.debug("using curl to update object %(obj), datastream %(dsid)s, from file %(file)s" % {'obj': obj, 'dsid': dsid, 'file': filename})
+        logging.debug("using curl to update object %(obj)s, datastream %(dsid)s, from file %(file)s" % {'obj': obj, 'dsid': dsid, 'file': filename})
         r = subprocess.call(['curl', '-i', '-H', '-XPOST', '%(url)s/objects/%(pid)s/datastreams/%(dsid)s?dsLabel=%(label)s&mimeType=%(mimetype)s&controlGroup=%(controlgroup)s'
                            % {'url': conn.url, 'pid': obj.pid, 'dsid': dsid, 'label': quote(label), 'mimetype': mimeType, 'controlgroup': controlGroup }, 
                            '-F', 'file=@%(filename)s' % {'filename': filename}, '-u', '%(username)s:%(password)s' % {'username': conn.username, 'password': conn.password}])
     
-        if not r == 0
+        if not r == 0:
             logging.error("error in curl code %(code)s" % {'code': r})
 
     except Exception as e:
         logging.error('exception in curl call: ' + str(e))
-
+        r = 1
     
     return r == 0
 
